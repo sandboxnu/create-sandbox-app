68d465d4343de25e0d7611afe93c75b5603e4d94
dajinchu <dajinchu@gmail.com>
2020-05-15 17:50:09 -0400

    test api

--- templates/node-fullstack-websocket/server/src/index.ts
+++ templates/node-fullstack-websocket/server/src/index.ts
@@ -1,10 +1,7 @@
 import "reflect-metadata";
-import { createConnection } from "typeorm";
 import path from "path";
-import Hapi from '@hapi/hapi';
-import io from 'socket.io'
-import { clubRoutes } from "./api/clubRoutes";
-import websocketManager from "./websocketManager";
+import { start } from "./server";
+import { createConnection } from "typeorm";
 
 const DB_URL = process.env.DB_URL || "postgres://postgres@localhost:5432/dev";
 
@@ -18,20 +15,7 @@ async function main() {
     migrations: [path.resolve(__dirname, "migration/**/*{.js,.ts}")],
     subscribers: [path.resolve(__dirname, "subscriber/**/*{.js,.ts}")],
   });
-
-  const server = Hapi.server({
-    port: 3002,
-    host: 'localhost'
-  })
-
-  // Add routes
-  server.route(clubRoutes)
-
-  // Bind socketio to http server
-  websocketManager.bindSocketIO(io(server.listener))
-
-  await server.start();
-  console.log("> Server up");
+  await start();
 }
 
 main().catch((e) => console.log(e));
